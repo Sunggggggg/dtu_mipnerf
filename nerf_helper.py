effd23c298ced5472fa31f837f8c73588fe8b4e2
Sunggggggg <sky990319@gmail.com>
2024-01-30 16:05:13 +0900

    f

--- nerf_helper.py
+++ nerf_helper.py
@@ -26,11 +26,11 @@ def get_rays_dtu(H, W, p2c, c2w):
     p2c : [3, 3]
     c2w : [3, 4]
     """
-    x, y = torch.meshgrid(torch.arange(H) + 0.5, torch.arange(W) + 0.5, indexing='xy')
-    x = x.t()
-    y = y.t()
+    i, j = torch.meshgrid(torch.arange(W) + 0.5, torch.arange(H) + 0.5)
+    i = i.t()
+    j = j.t()
 
-    ray_dirs = torch.stack([x, y, torch.ones_like(x)], -1)           # [H, W, 3]
+    ray_dirs = torch.stack([i, j, torch.ones_like(i)], -1)           # [H, W, 3]
     cam_dirs = ray_dirs @ p2c.T                                      # [H, W, 3] * [3, 3]
     rays_d = torch.sum(cam_dirs[..., None, :] * c2w[:3, :3], -1)     # 
     rays_o = c2w[:3,-1].expand(rays_d.shape)               
--- run_mipnerf.py
+++ run_mipnerf.py
@@ -131,7 +131,6 @@ def train(rank, world_size, args):
         else:
             coords = torch.stack(torch.meshgrid(torch.linspace(0, H-1, H), torch.linspace(0, W-1, W)), -1)  # (H, W, 2)
 
-        print(coords.shape)
         coords = torch.reshape(coords, [-1,2])  # (H * W, 2)
         select_inds = np.random.choice(coords.shape[0], size=[N_rand], replace=False)  # (N_rand,)
         select_coords = coords[select_inds].long()        # (N_rand, 2)
