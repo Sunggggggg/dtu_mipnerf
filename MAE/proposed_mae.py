79ea12c5422936410733ab87282149862ae37631
Sunggggggg <sky990319@gmail.com>
2024-02-06 17:38:09 +0900

    f

--- MAE/proposed_mae.py
+++ MAE/proposed_mae.py
@@ -10,7 +10,7 @@ class ResnetEmbed(nn.Module):
     def __init__(self, dim=2048):
         super().__init__()
         self.dim = dim
-        resnet = models.resnet34(pretrained=True)
+        self.resnet = models.resnet34(pretrained=True)
         
         # for param in resnet.parameters():
         #     param.requires_grad = False
@@ -149,7 +149,7 @@ class ProposedMAE(nn.Module):
         # with torch.no_grad():
         #     x = self.embed(x)     #[B, N, embed_dim]
         x = self.embed(x)
-        
+
         # Positional encoding
         if self.cam_pose_encoding :
             pos_embed = get_3d_sincos_pos_embed(poses, x.shape[-1], True).to(x.device)
