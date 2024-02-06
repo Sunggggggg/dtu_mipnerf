5fd59f1d6d435c000d7dfd14bff71ad64dcc05f8
Sunggggggg <sky990319@gmail.com>
2024-02-06 13:59:43 +0900

    f

--- MAE/__init__.py
+++ MAE/__init__.py
@@ -1,4 +1,4 @@
-from .model_mae import IMAGE, PATCH, IMAGE_MAE, PATCH_MAE, ProposedMAE
+from .model_mae import IMAGE, PATCH, IMAGE_MAE, PATCH_MAE
 from .mae_helper import mae_input_format, make_input
 from .visualize import to8b, image_plot
 from .proposed_mae import PRO_MAE
